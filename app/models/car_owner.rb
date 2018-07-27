29b121385a15d804663b75a09038239a3fca6b86
Jeff Crockett <jeffreyneilcrockett@gmail.com>
2018-07-27 09:48:53 -0400

    Initial Commit

--- app/models/car_owner.rb
+++ app/models/car_owner.rb
@@ -1,10 +1,33 @@
 class CarOwner
-
+  @@all = [] 
   attr_reader :name
+
+  def self.all 
+    @@all 
+  end
 
   def initialize(name)
     @name = name
+    @@all << self 
   end
 
+  def cars 
+    Car.all.select do |car|
+      car.car_owner == self 
+    end
+  end
 
+  def mechanics 
+    cars.map do |car|
+      car.mechanic 
+    end
+  end
+
+  def self.average_cars_owned
+    sum = 0 
+    self.all.each do |car_owner|
+      sum += car_owner.cars.length 
+    end
+    sum / self.all.length 
+  end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,20 @@ def reload
   load 'config/environment.rb'
 end
 
+
+co1 = CarOwner.new("Jeff")
+co2 = CarOwner.new("Peter")
+co3 = CarOwner.new("Aubree")
+co4 = CarOwner.new("Molly")
+
+m1 = Mechanic.new("Jenn", "Sedan")
+m2 = Mechanic.new("Rob", "Sport")
+m3 = Mechanic.new("Sam", "Coupe")
+m4 = Mechanic.new("John", "Minivan")
+
+c1 = Car.new("Nissan", "Versa", "Sedan", co1, m1)
+c2 = Car.new("Chevy", "Corvette", "Sport", co2, m2)
+c3 = Car.new("Toyota", "Previa", "Minivan", co3, m3)
+c4 = Car.new("Toyota", "Corolla", "Coupe", co4, m4)
+
 binding.pry
